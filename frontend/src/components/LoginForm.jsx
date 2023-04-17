8c2409c8cafc90970ebcbfce53fd1bc3928c0338
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-04-17 08:01:17 -0700

    update newsletter

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -20,14 +20,14 @@ function LoginForm() {
   }
 
   return (
-    <div className='bg-slate-200 py-10 px-12 rounded-md shadow-lg m-4'>
-      <h1 className='md:text-4xl text-3xl text-slate-800 font-semibold'>Welcome Back</h1>
-      <p className='font-medium text-lg text-slate-800 md:mt-3 mt-2'>Please enter your details.</p>
-      <form className='md:mt-5 mt-4' onSubmit={handleSubmit}>
+    <div className='bg-slate-200 py-10 landscape:py-4 px-12 rounded-md shadow-lg m-4'>
+      <h1 className='md:text-4xl landscape:text-2xl text-3xl text-slate-800 font-semibold'>Welcome Back</h1>
+      <p className='font-medium text-lg text-slate-800 md:mt-3 mt-2 landscape:mt-1'>Please enter your details.</p>
+      <form className='md:mt-5 mt-4 landscape:mt-1' onSubmit={handleSubmit}>
         <div>
           <div className=''>
-            <label className='text-slate-800 text-lg font-medium w-full' htmlFor="email">Email</label>
-            <input className='w-full border-2 border-slate-300 text-slate-900 text-lg font-medium bg-slate-50 rounded p-1 px-2 mt-1'
+            <label className='text-slate-800 text-lg landscape:text-base font-medium w-full' htmlFor="email">Email</label>
+            <input className='w-full border-2 border-slate-300 text-slate-900 text-lg landscape:text-base font-medium bg-slate-50 rounded p-1 px-2 mt-1'
               onChange={handleEmailChange}
               value={email}
               type="email"
@@ -37,9 +37,9 @@ function LoginForm() {
               required
             />
           </div>
-          <div className='mt-2'>
-            <label className='text-slate-800 text-lg font-medium w-full' htmlFor="password">Password</label>
-            <input className='w-full border-2 border-slate-300 text-slate-900 text-lg font-medium bg-slate-50 rounded p-1 px-2 mt-1'
+          <div className='mt-2 landscape:mt-1'>
+            <label className='text-slate-800 text-lg landscape:text-base font-medium w-full' htmlFor="password">Password</label>
+            <input className='w-full border-2 border-slate-300 text-slate-900 text-lg landscape:text-base font-medium  bg-slate-50 rounded p-1 px-2 mt-1'
               onChange={handlePasswordChange}
               value={password}
               type="password"
@@ -50,9 +50,12 @@ function LoginForm() {
             />
           </div>
           <div>
-            <button className='bg-slate-700 text-slate-100 rounded text-lg w-full mt-6 px-6 py-2 hover:bg-slate-600 transition duration-300 ease-in-out'>
+            <button className='bg-slate-700 text-slate-100 rounded text-lg w-full mt-6 landscape:mt-4 px-6 py-2 landscape:py-1 hover:bg-slate-600 transition duration-300 ease-in-out'>
               Login
             </button>
+          </div>
+          <div>
+            <p className='text-slate-800 text-lg font-medium mt-4'>Don't have an account? <a href='/signup' className='text-slate-700 hover:text-slate-600'>Sign Up</a></p>
           </div>
         </div>
       </form>
