997532195459393c847b9a7f3364b92cf4bbfa6a
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-04-18 03:13:46 -0700

    add registration form

--- frontend/src/pages/RegisterEmployer.jsx
+++ frontend/src/pages/RegisterEmployer.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import RegisterEmployerForm from '../components/RegisterEmployerForm';
 
 function RegisterEmployer() {
-    
-    return (
-        <div className='flex w-full h-screen bg-slate-800'>
-            <div className='w-full flex items-center justify-center'>
-                <RegisterEmployerForm />
-            </div>
-        </div>
-    )
+
+  return (
+    <div className='flex w-full h-screen bg-slate-800'>
+      <div className='w-full flex items-center justify-center'>
+        <RegisterEmployerForm />
+      </div>
+    </div>
+  )
 }
 
 export default RegisterEmployer;
