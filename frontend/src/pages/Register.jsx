edd67ad02ae3ee60a5209c2e80bd977d5234b58e
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-04-17 19:38:25 -0700

    register page

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Register() {
+  return (
+    <div className='flex w-full h-screen bg-slate-800'>
+      <div className='w-full flex items-center justify-center'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Register;
