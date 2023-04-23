555a7cce1aacd25c9a40b1b1be434446b9186596
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-04-23 01:34:01 -0700

    update dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -21,6 +21,7 @@ const Dashboard = () => {
       } catch (error) {
         localStorage.removeItem('authToken');
         alert('You are not authorized. Please login.');
+        window.location.href = '/login';
       }
     };
     fetchPrivateData();
