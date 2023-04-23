2af98fc78ab4fd746975675b5ccc5672a3e9dada
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-04-22 20:05:51 -0700

    add all auth functionality, private route

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -29,7 +29,7 @@ const Dashboard = () => {
   return (
     <div className='w-full py-16 px-4 shadow-lg bg-slate-200 mt-20'>
       <div className='max-w-[1250px] mx-auto'>
-        <h1 className='text-4xl font-semibold text-slate-800'>Dashboard</h1>
+        <h1 className='text-4xl font-semibold text-slate-800'>Welcome {employer.name}</h1>
         <div className='flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 mt-8'>
           <div className='bg-slate-100 rounded-md shadow-lg p-4 w-full'>
             This is the dashboard. Only visible if logged in.
