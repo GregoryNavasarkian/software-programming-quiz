d2c81415b1881919e3e820121e9c2fa163580734
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-04-22 21:56:06 -0700

    add testing to backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
+  const connection = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  console.log('MongoDB connected');
-
+  if (!connection) {
+    console.log('MongoDB connection failed');
+  }
 }
 
 module.exports = connectDB;
---
+++ backend/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testMatch: ['**/test/**/*.test.js'],
+  testEnvironment: 'node',
+  testTimeout: 30000,
+  verbose: true
+};
