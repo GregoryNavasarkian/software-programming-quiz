31710a90c5edd12ddefe03829af1fc92307cda57
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-04-24 20:53:03 -0700

    add quiz schema

--- backend/models/Employer.js
+++ backend/models/Employer.js
@@ -26,6 +26,12 @@ const EmployerSchema = new mongoose.Schema(
     },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
+    quizzes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Quiz'
+      }
+    ]
 
   },
   {
---
+++ backend/models/Quiz.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+
+const QuizSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please add a title']
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Employer',
+      required: true
+    },
+    // each question has a question type, question text, choices, and correct answer(s)
+    questions: [
+      {
+        questionType: {
+          type: String,
+          enum: ['multiple-choice', 'true-false', 'short-answer', 'select-all'],
+          required: [true, 'Please add a question type']
+        },
+        questionText: {
+          type: String,
+          required: [true, 'Please add a question']
+        },
+        choices: [
+          {
+            type: String,
+            required: [true, 'Please add a choice']
+          }
+        ],
+        correctAnswers: [
+          {
+            type: String,
+          }
+        ]
+      }
+    ],
+    // each quiz has a time limit
+    timeLimit: {
+      type: Number,
+      required: [true, 'Please add a time limit']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    database: "software-quiz",
+    collection: "quizzes"
+  }
+);
+
+const Quiz = mongoose.model('Quiz', QuizSchema);
+
+module.exports = Quiz;
