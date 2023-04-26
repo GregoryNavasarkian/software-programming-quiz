03400e88d9bfed3501fb850df9e998ed96de4753
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-04-26 02:01:49 -0700

    update dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -83,7 +83,7 @@ const Dashboard = () => {
                 <Link to='/create-quiz'>
                   <button
                     type="button"
-                    className="inline-flex items-center rounded-md bg-slate-700 px-6 py-2 text-base font-semibold text-slate-100 shadow-sm hover:bg-slate-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
+                    className="inline-flex items-center rounded-md bg-slate-700 px-6 py-2 text-base font-semibold text-slate-100 shadow-sm hover:bg-slate-600 transition duration-300 ease-in-out"
                   >
                     Create Quiz
                   </button>
