ce5288f2d692cfdaa8a6aaed054f717b1fbc978e
Gregory Navasarkian <gregory.navasarkian@gmail.com>
2023-05-09 23:37:52 -0700

    update quiz card

--- frontend/src/components/QuizCard.jsx
+++ frontend/src/components/QuizCard.jsx
@@ -1,6 +1,5 @@
-import { React, useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 const QuizCard = ({ quiz }) => {  
 
